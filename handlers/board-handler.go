371d38b94186b45c59de9d96c0f4504b39c0a91d
thisisnew <thisisnew@naver.com>
2021-06-09 23:06:28 +0900

    main 생성 (진행중)

--- handlers/board-handler.go
+++ handlers/board-handler.go
@@ -1,10 +1,34 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+	"practice-news-board-web/database"
 )
 
+type Board struct {
+	BoardId      uint   `json:"boardId"`
+	BoardName    string `json:"boardName"`
+	BoardExplain string `json:"boardExplain"`
+	BoardNo      string `json:"boardNo"`
+	BoardState   uint   `json:"boardState"`
+}
+
 func GetBoardList(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "board List")
+	db := database.GetDB()
+
+	var board []Board
+	db.Table("company").Find(&board)
+
+	//var items Companies
+	//items.Companies = companies
+	//items.PageInfo = PageInfo{
+	//	TotalRecord: 1,
+	//	TotalPage:   1,
+	//	Limit:       15,
+	//	Page:        1,
+	//	PrevPage:    1,
+	//	NextPage:    1,
+	//}
+	//
+	//json.NewEncoder(w).Encode(items)
 }
