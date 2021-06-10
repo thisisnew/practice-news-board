1058421becc34b876e62d7f9927ba9739bc87a00
thisisnew <thisisnew@naver.com>
2021-06-10 23:12:56 +0900

    main 생성 (진행중)

--- handlers/board-handler.go
+++ handlers/board-handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"practice-news-board-web/database"
 	"practice-news-board-web/messages"
@@ -14,8 +15,23 @@ func GetBoardList(w http.ResponseWriter, r *http.Request) {
 	var board []models.Board
 	db.Table(models.Board{}.Table()).Find(&board)
 
+	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/item/126809.json?print=pretty")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var news messages.News
+	json.Unmarshal([]byte(data), &news)
+
 	var items messages.Board
 	items.Item = board
+	items.News = news
 
 	json.NewEncoder(w).Encode(items)
 }
--- messages/board-message.go
+++ messages/board-message.go
@@ -4,4 +4,17 @@ import "practice-news-board-web/models"
 
 type Board struct {
 	Item []models.Board
+	News News
+}
+
+type News struct {
+	By          string
+	Descendants uint
+	Id          string
+	Kids        []uint
+	Parts       []uint
+	Score       uint
+	Time        uint
+	Title       string
+	Type        string
 }
