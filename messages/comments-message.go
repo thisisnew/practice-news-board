fd0fd3c0a4b5e1cb10f0e6df296f4e810a5bf6db
thisisnew <thisisnew@naver.com>
2021-06-15 23:09:22 +0900

    댓글리스트

--- messages/comments-message.go
+++ messages/comments-message.go
@@ -1,8 +1,13 @@
 package messages
 
+import "time"
+
 type Comment struct {
-	CommentId   string `json:"commentId"`
-	Commenter   string `json:"boardName"`
-	CommentHide bool   `json:"commentHide"`
-	BoardId     string `json:"boardId"`
+	CommentId   string    `json:"commentId"`
+	Commenter   string    `json:"commenter"`
+	Contents    string    `json:"contents"`
+	CreateDate  time.Time `json:"create_date"`
+	UpdateDate  time.Time `json:"update_date"`
+	CommentHide bool      `json:"commentHide"`
+	BoardId     string    `json:"boardId"`
 }
--- models/comments.go
+++ models/comments.go
@@ -1,13 +1,17 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
 
 type Comment struct {
-	gorm.Model
-	CommentId   string `json:"commentId"`
-	Commenter   string `json:"boardName"`
-	CommentHide bool   `json:"commentHide"`
-	BoardId     string `json:"boardId"`
+	CommentId   string
+	Commenter   string
+	CommentHide bool
+	Contents    string
+	CreateDate  time.Time
+	UpdateDate  time.Time
+	BoardId     string
 }
 
 func (Comment) Table() string {
