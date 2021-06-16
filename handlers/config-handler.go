4a66952334fde490e0055ef31d0fe65cd240955c
thisisnew <thisisnew@naver.com>
2021-06-17 00:06:01 +0900

    전체적인 구조 재구성

--- handlers/config-handler.go
+++ handlers/config-handler.go
@@ -4,14 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 	"practice-news-board-web/messages"
-	"practice-news-board-web/processors"
 )
 
 func GetBoardList(w http.ResponseWriter, r *http.Request) {
-	list := processors.GetBoardList()
-
 	var items messages.BoardList
-	items.Items = list
 
 	json.NewEncoder(w).Encode(items)
 }
--- processors/config-processor.go
+++ processors/config-processor.go
@@ -2,14 +2,13 @@ package processors
 
 import (
 	"practice-news-board-web/database"
-	"practice-news-board-web/messages"
 	"practice-news-board-web/models"
 )
 
-func GetBoardList() []messages.Board {
+func GetBoardList() []models.Board {
 	db := database.GetDB()
 
-	var board []messages.Board
+	var board []models.Board
 	db.Table(models.Board{}.Table()).Find(&board)
 
 	return board
